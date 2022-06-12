e4dde21bd30f2d42cfd67780c61c64f9a8e438e2
Thomas Taylor <thmsmtylr@gmail.com>
2022-06-12 15:24:18 +1000

    Start Annoucement component

---
+++ components/Annoucement.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export const Annoucement = () => {
+  return (
+    <div className="absolute top-0 left-0 w-full p-4 dark:bg-black/90 flex items-center justify-center z-10 text-sm font-medium">
+      <span className="dark:text-fuchsia-500 animate-pulse">
+        ✨✨ Live on Twitch ✨✨
+      </span>
+    </div>
+  );
+};
---
+++ components/index.tsx
@@ -0,0 +1,6 @@
+import { Annoucement } from "../components/Annoucement";
+import { Card } from "../components/Card";
+import { Footer } from "../components/Footer";
+import { SpotifyCurrentlyPlaying } from "../components/SpotifyCurrentlyPlaying";
+
+export { Annoucement, Card, Footer, SpotifyCurrentlyPlaying };
