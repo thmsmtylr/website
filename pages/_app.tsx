cfc1af0da80ab82f5ad8f19f1f7f2f3465673caa
Thomas Taylor <thmsmtylr@gmail.com>
2022-06-14 14:40:31 +1000

    Add page tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,34 @@
-import * as React from "react";
-import type { AppProps } from "next/app";
+import React, { useEffect } from "react";
+import * as gtag from "../lib/gtag";
 import dynamic from "next/dynamic";
 import Script from "next/script";
+import { useRouter } from "next/router";
+import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Script
-      strategy="afterInteractive"
-      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-    />
-    <Script
-      id="gtag-init"
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => gtag.pageview(url);
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  console.log(gtag);
+
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
@@ -22,11 +36,12 @@ const App = ({ Component, pageProps }: AppProps) => (
           page_path: window.location.pathname,
         });
       `,
-      }}
-    />
-    <Component {...pageProps} />
-  </>
-);
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
+};
 
 export default dynamic(() => Promise.resolve(App), {
   ssr: false,
