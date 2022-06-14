b39fc8c4e063e395f4f213f32c95ddd03050973d
Thomas Taylor <thmsmtylr@gmail.com>
2022-06-14 12:04:45 +1000

    Fix footer light mode legibility and add link aria label

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,12 +2,13 @@ import * as React from "react";
 
 export const Footer = () => {
   return (
-    <footer className="fixed w-full text-xs p-4 bottom-0 left-0 text-center text-white/40">
+    <footer className="fixed w-full text-xs p-4 bottom-0 left-0 text-center dark:text-white/40 text-gray-300">
       © {new Date().getFullYear()} thmsmtylr &middot;{" "}
       <a
         href="https://github.com/thmsmtylr/website"
         target="_blank"
         rel="noreferrer"
+        aria-label="Link to Github source code"
       >
         Sauce
       </a>
