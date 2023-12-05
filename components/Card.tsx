b63a0aec79624df1bbc8259bd81253ac856676f7
Thomas Taylor <thmsmtylr@gmail.com>
2023-12-05 22:28:35 +1100

    fix: tighten type

--- components/Card.tsx
+++ components/Card.tsx
@@ -15,8 +15,8 @@ const buttonLinks: ButtonLinkProps = [
 ];
 
 export const Card = () => (
-  <div className="relative dark:bg-black/90 bg-white rounded-xl shadow-lg px-4 py-7 max-w-[340px] min-w-[340px] space-y-4 mb-4">
-    <div className="rounded-full w-20 h-20 relative mx-auto">
+  <div className="relative dark:bg-black/90 bg-white rounded-xl shadow-lg px-4 py-7 max-w-[340px] min-w-[340px] flex flex-col items-center mb-4">
+    <div className="rounded-full w-20 h-20 relative mx-auto mb-3">
       <Image
         src={Avatar}
         layout="fill"
@@ -24,22 +24,24 @@ export const Card = () => (
         alt="Thomas Taylor avatar"
       />
     </div>
-    <h1 className="font-inter text-base dark:text-white text-center font-medium">
-      Thomas Taylor
-    </h1>
-    <p className="font-inter text-sm dark:text-white text-center">
-      UI Engineer{" "}
-      <a
-        className="text-rose-400 hover:text-fuchsia-500 transition-colors"
-        href="https://www.swell.is/"
-        target="_blank"
-        rel="noreferrer"
-        aria-label="Alembic"
-      >
-        @swell
-      </a>{" "}
-      💜
-    </p>
+    <div className="mb-6">
+      <h1 className="font-inter text-base dark:text-white text-center font-bold">
+        Thomas Taylor
+      </h1>
+      <p className="font-inter text-sm dark:text-white text-center">
+        UI Engineer{" "}
+        <a
+          className="text-rose-400 hover:text-fuchsia-500 transition-colors"
+          href="https://www.swell.is/"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="Everything's swell"
+        >
+          @swell
+        </a>{" "}
+        💜
+      </p>
+    </div>
     {buttonLinks.map((link) => (
       <a
         className="w-full font-medium hover:border-fuchsia-500 rounded-xl inline-block p-2 dark:text-white hover:text-fuchsia-500 border dark:border-white border-black text-center transition-colors"
